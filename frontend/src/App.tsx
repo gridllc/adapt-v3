ffb030ac0ffd3d46bf7945cdcca7b9488751e98f
gridllc <patrick@gridllc.net>
2025-08-11 15:23:40 -0700

    clerk1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { useAuth } from '@clerk/clerk-react'
+import { useAuth, SignIn } from '@clerk/clerk-react'
 import { ProtectedRoute } from '@components/common/ProtectedRoute'
 import { Layout } from '@components/common/Layout'
 import { HomePage } from '@pages/HomePage'
@@ -58,7 +58,8 @@ function App() {
         {/* Always show home page at root */}
         <Route path="/" element={<HomePage />} />
         
-        {/* Clerk authentication routes */}
+        {/* Clerk authentication routes - for debugging with hosted portal */}
+        <Route path="/sign-in" element={<SignIn routing="path" path="/sign-in" />} />
         <Route path="/sso-callback" element={<div>Loading...</div>} />
 
         {/* Protected routes */}
