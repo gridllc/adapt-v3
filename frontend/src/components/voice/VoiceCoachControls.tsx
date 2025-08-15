b6588eaabb31abee0ed5d3bd77976ab6d29af888
gridllc <patrick@gridllc.net>
2025-08-15 11:15:14 -0700

    phone5

--- frontend/src/components/voice/VoiceCoachControls.tsx
+++ frontend/src/components/voice/VoiceCoachControls.tsx
@@ -1,5 +1,19 @@
 // TEMP hotfix: disable Voice Coach controls entirely
-export default function VoiceCoachControls(props: any) {
+interface VoiceCoachControlsProps {
+  steps: any[];
+  currentStepIndex: number;
+  onStepChange: (index: number) => void;
+  onSeek: (time: number) => void;
+  onPause: () => void;
+  onPlay: () => void;
+  getCurrentTime?: () => number;
+  onMute?: () => void;
+  onUnmute?: () => void;
+  speechOptions?: any;
+  pressToTalk?: boolean;
+}
+
+export default function VoiceCoachControls(props: VoiceCoachControlsProps) {
   return null;
 }
 export { VoiceCoachControls };
--- frontend/src/components/voice/VoiceCoachOverlay.tsx
+++ frontend/src/components/voice/VoiceCoachOverlay.tsx
@@ -1,5 +1,12 @@
 // TEMP hotfix: disable Voice Coach overlay entirely
-export function VoiceCoachOverlay(props: any) {
+interface VoiceCoachOverlayProps {
+  isVisible: boolean;
+  onStart: () => void;
+  onDismiss: () => void;
+  dismissOnBackdropClick?: boolean;
+}
+
+export function VoiceCoachOverlay(props: VoiceCoachOverlayProps) {
   return null;
 }
 export default VoiceCoachOverlay;
